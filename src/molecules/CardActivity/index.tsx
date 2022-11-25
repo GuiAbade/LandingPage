c712023e7ecae123299d5cb70fcf2b0aba4a040b
GuiAbade <abade.guim13@gmail.com>
2022-11-24 22:32:32 -0300

    Concluindo configuração e estilização da Activity

--- src/molecules/CardActivity/index.tsx
+++ src/molecules/CardActivity/index.tsx
@@ -5,8 +5,12 @@ const CardActivity = () => (
   <S.Container>
     <small>Activity</small>
     <div>
-     <span>18</span> 
+      <span>18</span> 
       <FiArrowUp size={25} color="#FFF"/></div>
+
+      <i className='circle1'></i>
+      <i className='circle2'></i>
+      <i className='circle3'></i>
   </S.Container>
 
 );
--- src/molecules/CardActivity/styles.ts
+++ src/molecules/CardActivity/styles.ts
@@ -11,6 +11,7 @@ export const Container = styled.div`
   border-radius: 12px;
   text-align: left;
   padding: 11px;
+  overflow: hidden;
 
   small {
     font-size: 15px;
@@ -24,6 +25,41 @@ export const Container = styled.div`
       font-size: 25px;
       font-weight: 400;
       margin-right: 5px;
+    }
+  }
+  
+  i{
+    width: 150px;
+    height: 77px;
+    position: absolute;
+    border-radius: 55px;
+
+    &.circle1{
+      left: 104px;
+      top: -20px;
+      background: linear-gradient(
+        0deg,
+        rgba(255, 255, 255, 0.3),
+        rgba(255, 255, 255, 0.3)
+      );
+    }
+
+    &.circle2 {
+      background: linear-gradient(
+        0deg, 
+        rgba(255, 255, 255, 0.3),
+        rgba(255, 255, 255, 0.3)
+        );
+      left: 81px;
+      bottom: -50px;
+      transform: rotate()(45deg);
+    }
+    
+    &.circle3 {
+      border: 1px solid rgba(255, 255, 255, 0.5);
+      transform: rotate(-72deg);
+      top: -54px;
+      left: 47px;
     }
   }
 `;
