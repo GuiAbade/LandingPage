209b0d78293bc95f1b88e4fcb6df8d35f42d0c2d
GuiAbade <abade.guim13@gmail.com>
2022-12-20 11:06:54 -0300

    Concluindo animação dos status CardChart

--- src/molecules/CardChart/index.tsx
+++ src/molecules/CardChart/index.tsx
@@ -1,28 +1,37 @@
-
-import { useCallback } from 'react';
-import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
-import * as S from './styles';
+import { useCallback } from "react";
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
+import * as S from "./styles";
 
 const CardChart = () => {
-    const amountBars = Array.from(Array(12).keys());
+  const amountBars = Array.from(Array(12).keys());
+
+  const fillZero = useCallback((value: number) => {
+    const number = (value + 1).toString();
+
+    return number.padStart(2, "0");
+  }, []);
 
   return (
     <S.Container>
       <div>
-        <FiArrowLeft/>
+        <FiArrowLeft />
         <strong>Stats</strong>
-        <FiArrowRight/>
+        <FiArrowRight />
       </div>
 
       <S.Chart>
-        {amountBars.map((item)=> (
-        <div key={String(item)}>
-          <span></span>
-        </div>
+        {amountBars.map((item) => (
+          <div key={String(item)}>
+            <div>
+              <span></span>
+            </div>
+
+            <small>{fillZero(item)}</small>
+          </div>
         ))}
       </S.Chart>
     </S.Container>
   );
-}
+};
 
 export default CardChart;
